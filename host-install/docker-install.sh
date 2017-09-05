eb5dc3027629b1a13ae1c9e993e7893cce16328a
jeppeny <jeppe.nylokke@ifad.dk>
2017-09-05 11:04:38 +0200

    Bugfix

--- host-install/docker-install.sh
+++ host-install/docker-install.sh
@@ -1,6 +1,6 @@
 #/bin/bash -v
 
-echo Download and install docker (ce version)
+echo "Download and install docker (ce version)"
 sudo apt-get update
 sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
