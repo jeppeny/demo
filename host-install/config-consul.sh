da129c34726979c81c77a4d371fd78e75d4f1504
jeppeny <jeppe.nylokke@ifad.dk>
2017-09-06 13:14:17 +0200

    Added files for setup of 5 nodes

---
+++ host-install/config-consul.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# AWS: Should be run on nodes 2-5
+
+if  [ -f docker-consul.conf ]; then
+	sudo mkdir -p /etc/systemd/system/docker.service.d
+	mv docker-consul /etc/systemd/system/docker.service.d
+
+	sudo systemctl daemon-reload
+	sudo systemctl restart docker
+
+	echo Check settings:
+	sudo systemctl show --property=ExecStart docker
+else
+	echo docker-consul.conf not found, exiting...
+	exit 1
+fi
